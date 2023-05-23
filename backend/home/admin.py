49d62d6e27efed3540b364a533457b9f4897db45
crowdbotics-dev <engineering@crowdbotics.com>
2023-05-23 21:40:16 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import NewMo,Rojo
+admin.site.register(NewMo)
+admin.site.register(Rojo)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,10 @@
+from django.conf import settings
 from django.db import models
+class NewMo(models.Model):
+    'Generated Model'
+    value = models.BigIntegerField()
+class Rojo(models.Model):
+    'Generated Model'
+    name = models.BigIntegerField()
+    value2 = models.BigIntegerField()
+    valuew = models.BigIntegerField()
