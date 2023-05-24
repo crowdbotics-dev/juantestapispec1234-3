015bde5290712b02d5d13af517a916f0b4d87128
Crowdbotics <engineering@crowdbotics.com>
2023-05-24 20:54:43 +0000

    Update store.

--- store/juantestapispecAPI/api.js
+++ store/juantestapispecAPI/api.js
@@ -3,8 +3,20 @@ const juantestapispecAPI = axios.create({
   baseURL: "https://juantestapispec1234-3.botics.co",
   headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
+function api_v1_rojo_list(payload) {
+  return juantestapispecAPI.get(`/api/v1/rojo/`)
+}
+function api_v1_rojo_create(payload) {
+  return juantestapispecAPI.post(`/api/v1/rojo/`, payload.data)
+}
 function api_v1_login_create(payload) {
   return juantestapispecAPI.post(`/api/v1/login/`, payload.data)
+}
+function api_v1_newmo_list(payload) {
+  return juantestapispecAPI.get(`/api/v1/newmo/`)
+}
+function api_v1_newmo_create(payload) {
+  return juantestapispecAPI.post(`/api/v1/newmo/`, payload.data)
 }
 function api_v1_signup_create(payload) {
   return juantestapispecAPI.post(`/api/v1/signup/`, payload.data)
@@ -26,11 +38,35 @@ function api_docs_schema_retrieve(payload) {
 function rest_auth_login_create(payload) {
   return juantestapispecAPI.post(`/rest-auth/login/`, payload.data)
 }
+function api_v1_rojo_retrieve(payload) {
+  return juantestapispecAPI.get(`/api/v1/rojo/${payload.id}/`)
+}
+function api_v1_rojo_update(payload) {
+  return juantestapispecAPI.put(`/api/v1/rojo/${payload.id}/`, payload.data)
+}
+function api_v1_rojo_partial_update(payload) {
+  return juantestapispecAPI.patch(`/api/v1/rojo/${payload.id}/`, payload.data)
+}
+function api_v1_rojo_destroy(payload) {
+  return juantestapispecAPI.delete(`/api/v1/rojo/${payload.id}/`)
+}
 function rest_auth_logout_retrieve(payload) {
   return juantestapispecAPI.get(`/rest-auth/logout/`)
 }
 function rest_auth_logout_create(payload) {
   return juantestapispecAPI.post(`/rest-auth/logout/`)
+}
+function api_v1_newmo_retrieve(payload) {
+  return juantestapispecAPI.get(`/api/v1/newmo/${payload.id}/`)
+}
+function api_v1_newmo_update(payload) {
+  return juantestapispecAPI.put(`/api/v1/newmo/${payload.id}/`, payload.data)
+}
+function api_v1_newmo_partial_update(payload) {
+  return juantestapispecAPI.patch(`/api/v1/newmo/${payload.id}/`, payload.data)
+}
+function api_v1_newmo_destroy(payload) {
+  return juantestapispecAPI.delete(`/api/v1/newmo/${payload.id}/`)
 }
 function rest_auth_registration_create(payload) {
   return juantestapispecAPI.post(`/rest-auth/registration/`, payload.data)
@@ -78,15 +114,27 @@ function rest_auth_registration_verify_email_create(payload) {
   )
 }
 export const apiService = {
+  api_v1_rojo_list,
+  api_v1_rojo_create,
   api_v1_login_create,
+  api_v1_newmo_list,
+  api_v1_newmo_create,
   api_v1_signup_create,
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update,
   api_docs_schema_retrieve,
   rest_auth_login_create,
+  api_v1_rojo_retrieve,
+  api_v1_rojo_update,
+  api_v1_rojo_partial_update,
+  api_v1_rojo_destroy,
   rest_auth_logout_retrieve,
   rest_auth_logout_create,
+  api_v1_newmo_retrieve,
+  api_v1_newmo_update,
+  api_v1_newmo_partial_update,
+  api_v1_newmo_destroy,
   rest_auth_registration_create,
   modules_articles_article_list,
   modules_articles_article_create,
